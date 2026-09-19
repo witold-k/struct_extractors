b4cd16bb084cb3703f3987294c513d2fff152269
witold-k <85440409+witold-k@users.noreply.github.com>
2026-09-19 19:44:35 +0200

    Test generic multi-field comparators

--- tests/comparators_test.rs
+++ tests/comparators_test.rs
@@ -4,23 +4,27 @@
 use struct_extractors::extract_comparators;
 
 #[extract_comparators]
-pub struct MyStruct {
-    #[extract_compare(mycmp)]
-    pub val: usize,
-    pub other: usize,
+pub struct MyStruct<T>
+where
+    T: Ord,
+{
+    #[extract_compare(compare_value)]
+    pub value: T,
+    #[extract_compare(compare_other)]
+    pub other: T,
 }
 
 #[test]
-fn test_sort_by_val() {
-    let mut v = [
-        MyStruct { val: 5, other: 100 },
-        MyStruct { val: 1, other: 200 },
-        MyStruct { val: 3, other: 300 },
+fn sorts_generic_struct_by_each_marked_field() {
+    let mut values = [
+        MyStruct { value: 5, other: 1 },
+        MyStruct { value: 1, other: 3 },
+        MyStruct { value: 3, other: 2 },
     ];
 
-    v.sort_by(MyStruct::mycmp);
+    values.sort_by(MyStruct::compare_value);
+    assert_eq!(values.iter().map(|x| x.value).collect::<Vec<_>>(), vec![1, 3, 5]);
 
-    let result: Vec<usize> = v.iter().map(|x| x.val).collect();
-    assert_eq!(result, vec![1, 3, 5]);
+    values.sort_by(MyStruct::compare_other);
+    assert_eq!(values.iter().map(|x| x.other).collect::<Vec<_>>(), vec![1, 2, 3]);
 }
-
