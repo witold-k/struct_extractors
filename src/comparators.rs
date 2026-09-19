00332abd2081b5906ed231e0097c0d69237abda6
witold-k <85440409+witold-k@users.noreply.github.com>
2026-09-19 19:44:04 +0200

    Preserve generics in comparator generation

--- src/comparators.rs
+++ src/comparators.rs
@@ -9,25 +9,27 @@ pub(crate) fn extract_compare_impl(_args: TokenStream, input: TokenStream) -> To
     input
 }
 
-/* -------------------------------------------------------------------------
-   STRUCT-LEVEL MACRO
-   ------------------------------------------------------------------------- */
-
 pub(crate) fn extract_comparators_impl(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mut ast = parse_macro_input!(input as DeriveInput);
     let struct_ident = &ast.ident;
-
-    let mut generated = quote! {};
+    let generics = ast.generics.clone();
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let mut methods = quote! {};
 
     if let syn::Data::Struct(ref mut data) = ast.data {
         for field in &mut data.fields {
             let mut comparator_name = None;
 
-            // Extract #[extract_compare(MyCmp)]
             field.attrs.retain(|attr| {
                 if attr.path().is_ident("extract_compare") {
-                    let args = attr.parse_args::<syn::Path>().unwrap();
-                    comparator_name = args.get_ident().cloned();
+                    let path = attr
+                        .parse_args::<syn::Path>()
+                        .expect("invalid #[extract_compare(name)] syntax");
+                    comparator_name = Some(
+                        path.get_ident()
+                            .cloned()
+                            .expect("expected identifier in #[extract_compare(name)]"),
+                    );
                     false
                 } else {
                     true
@@ -35,14 +37,11 @@ pub(crate) fn extract_comparators_impl(_args: TokenStream, input: TokenStream) -
             });
 
             if let Some(cmp_ident) = comparator_name {
-                let field_ident = field.ident.as_ref().unwrap();
-
-                generated.extend(quote! {
-                    impl #struct_ident {
-                        #[inline(always)]
-                        pub fn #cmp_ident(a: &Self, b: &Self) -> core::cmp::Ordering {
-                            a.#field_ident.cmp(&b.#field_ident)
-                        }
+                let field_ident = field.ident.as_ref().expect("named fields only");
+                methods.extend(quote! {
+                    #[inline]
+                    pub fn #cmp_ident(a: &Self, b: &Self) -> core::cmp::Ordering {
+                        a.#field_ident.cmp(&b.#field_ident)
                     }
                 });
             }
@@ -51,7 +50,10 @@ pub(crate) fn extract_comparators_impl(_args: TokenStream, input: TokenStream) -
 
     quote! {
         #ast
-        #generated
+
+        impl #impl_generics #struct_ident #ty_generics #where_clause {
+            #methods
+        }
     }
     .into()
 }
