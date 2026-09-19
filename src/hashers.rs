d7a54b522ca497612f8a138610e80a6d671506d8
witold-k <85440409+witold-k@users.noreply.github.com>
2026-09-19 19:44:08 +0200

    Support generic and multiple hash extractors

--- src/hashers.rs
+++ src/hashers.rs
@@ -12,7 +12,10 @@ pub(crate) fn extract_hash_impl(_args: TokenStream, input: TokenStream) -> Token
 pub(crate) fn extract_hashers_impl(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mut ast = parse_macro_input!(input as DeriveInput);
     let struct_ident = &ast.ident;
-    let mut generated = quote! {};
+    let generics = ast.generics.clone();
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let mut wrappers = quote! {};
+    let mut methods = quote! {};
 
     if let Data::Struct(ref mut data) = ast.data {
         for field in &mut data.fields {
@@ -30,32 +33,44 @@ pub(crate) fn extract_hashers_impl(_args: TokenStream, input: TokenStream) -> To
             if marked {
                 let field_ident = field.ident.as_ref().expect("named fields only");
                 let wrapper_ident = format_ident!("{}HashBy{}", struct_ident, field_ident);
+                let method_ident = format_ident!("hash_by_{}", field_ident);
 
-                generated.extend(quote! {
+                wrappers.extend(quote! {
                     #[derive(Clone, Copy, Debug)]
-                    pub struct #wrapper_ident<'a>(pub &'a #struct_ident);
+                    pub struct #wrapper_ident<'a, #impl_generics>(
+                        pub &'a #struct_ident #ty_generics
+                    ) #where_clause;
 
-                    impl core::cmp::PartialEq for #wrapper_ident<'_> {
+                    impl<'a, #impl_generics> core::cmp::PartialEq
+                        for #wrapper_ident<'a, #ty_generics>
+                    #where_clause
+                    {
                         #[inline]
                         fn eq(&self, other: &Self) -> bool {
                             self.0.#field_ident == other.0.#field_ident
                         }
                     }
 
-                    impl core::cmp::Eq for #wrapper_ident<'_> {}
+                    impl<'a, #impl_generics> core::cmp::Eq
+                        for #wrapper_ident<'a, #ty_generics>
+                    #where_clause
+                    {}
 
-                    impl core::hash::Hash for #wrapper_ident<'_> {
+                    impl<'a, #impl_generics> core::hash::Hash
+                        for #wrapper_ident<'a, #ty_generics>
+                    #where_clause
+                    {
                         #[inline]
                         fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
-                            self.0.#field_ident.hash(state);
+                            core::hash::Hash::hash(&self.0.#field_ident, state);
                         }
                     }
+                });
 
-                    impl #struct_ident {
-                        #[inline(always)]
-                        pub fn hash(&self) -> #wrapper_ident {
-                            #wrapper_ident(self)
-                        }
+                methods.extend(quote! {
+                    #[inline]
+                    pub fn #method_ident(&self) -> #wrapper_ident<'_, #ty_generics> {
+                        #wrapper_ident(self)
                     }
                 });
             }
@@ -64,7 +79,11 @@ pub(crate) fn extract_hashers_impl(_args: TokenStream, input: TokenStream) -> To
 
     quote! {
         #ast
-        #generated
+        #wrappers
+
+        impl #impl_generics #struct_ident #ty_generics #where_clause {
+            #methods
+        }
     }
     .into()
 }
