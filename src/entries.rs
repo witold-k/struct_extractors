0537ea654f8def1da0da6527f2f8c62be8eb6723
witold-k <85440409+witold-k@users.noreply.github.com>
2026-09-19 19:49:23 +0200

    Remove unused entry-check marker trait

--- src/entries.rs
+++ src/entries.rs
@@ -20,8 +20,6 @@ pub(crate) fn base_entries_impl(_attr: TokenStream, item: TokenStream) -> TokenS
         #[doc(hidden)]
         pub mod #module_ident {
             #(pub struct #variants;)*
-
-            pub trait ContainsAll<Other> {}
         }
     }
     .into()
